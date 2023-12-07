743db6d79551c1cc1df7f1d151d3f4251c3dcae3
Rijan Acharya <rijanacharya100@gmail.com>
2023-12-07 13:35:36 -0500

    merging  book page

--- controllers/displaybookController.js
+++ controllers/displaybookController.js
@@ -1,6 +1,6 @@
 const Book = require('../models/book');
-const Review=require('../models/reviews')
-const Customer=require('../models/customer')
+const Review = require('../models/reviews')
+const Customer = require('../models/customer')
 const fs = require('fs');
 const express = require('express');
 
@@ -15,70 +15,70 @@ async function displayAllBooks(req, res) {
     const books = await Book.find();
 
     // Render a view with all books
-    res.render('allBooks', { books });
+    res.render('combineBooks', { books });
   } catch (error) {
     // Handle errors and send an appropriate response
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 
-    
-  }
-  
-  // Controller method to display a detailed view of a specific book
-  async function displayBookDetail(req, res) {
-    try {
-      // Extract the book ID from the request parameters
-      const bookId = req.params.id;
-  
-      // Fetch the book with the specified ID from the database
-      const book = await Book.findById(bookId).populate({
-        path: 'reviews',
-        populate: { path: 'customer', select: 'username' } // Populate the 'customer' field with the 'username'
-      });
-  
-      // Render a view with the detailed information for the specific book
-      res.render('bookDetail', { book });
-    } catch (error) {
-      // Handle errors and send an appropriate response
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-    
+
+}
+
+// Controller method to display a detailed view of a specific book
+async function displayBookDetail(req, res) {
+  try {
+    // Extract the book ID from the request parameters
+    const bookId = req.params.id;
+
+    // Fetch the book with the specified ID from the database
+    const book = await Book.findById(bookId).populate({
+      path: 'reviews',
+      populate: { path: 'customer', select: 'username' } // Populate the 'customer' field with the 'username'
+    });
+
+    // Render a view with the detailed information for the specific book
+    res.render('eachBook', { book });
+  } catch (error) {
+    // Handle errors and send an appropriate response
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
 
-  async function postBookReview(req, res) {
-    try {
-      const bookId = req.params.id;
-      const { content } = req.body;
-  
-      // Assuming you have a user authentication system and a user ID available
-      const userId = 'your_user_id_here'; // Replace with the actual user ID
-  
-      // Create a new review
-      const newReview = new Review({
-        customer: '656f87f76a29999155ff87c7', // Associate the review with the authenticated user
-        book: bookId,
-        content,
-      });
-  
-      // Save the new review to the database
-      await newReview.save();
-  
-      // Associate the review with the book
-      const book = await Book.findById(bookId);
-      book.reviews.push(newReview._id);
-      await book.save();
-  
-      res.redirect(`/books/${bookId}`); // Redirect to the book reviews page
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
+}
+
+async function postBookReview(req, res) {
+  try {
+    const bookId = req.params.id;
+    const { content } = req.body;
+
+    // Assuming you have a user authentication system and a user ID available
+    const userId = 'your_user_id_here'; // Replace with the actual user ID
+
+    // Create a new review
+    const newReview = new Review({
+      customer: '656f87f76a29999155ff87c7', // Associate the review with the authenticated user
+      book: bookId,
+      content,
+    });
+
+    // Save the new review to the database
+    await newReview.save();
+
+    // Associate the review with the book
+    const book = await Book.findById(bookId);
+    book.reviews.push(newReview._id);
+    await book.save();
+
+    res.redirect(`/books/${bookId}`); // Redirect to the book reviews page
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
-  
-  module.exports = {
-    displayAllBooks,
-    displayBookDetail,
-    postBookReview,
-  };
+}
+
+module.exports = {
+  displayAllBooks,
+  displayBookDetail,
+  postBookReview,
+};
---
+++ public/script/search.js
@@ -0,0 +1,37 @@
+
+// Function to search books
+function searchBooks() {
+  const searchTerm = document.getElementById('searchTerm').value;
+  console.log('Search term:', searchTerm);
+
+  fetch(`/books/search?searchTerm=${searchTerm}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log('Received search results:', data);
+
+      const bookList = document.getElementById('bookList');
+      bookList.innerHTML = '';
+
+      data.results.forEach(book => {
+        console.log('Book:', book);
+        const listItem = document.createElement('li');
+        listItem.className = 'm-2';
+        listItem.innerHTML = `
+                                    <li class="m-2">
+                                      <div class="card" style="height: fit-content; width: fit-content;">
+                                        <img src="data:${book.image.contentType};base64,${book.image.data}" alt="${book.title}"
+                                          class="card-img-top" style="cursor: pointer; height: 150px;"
+                                          onclick="navigateToBookDetail('${book._id}')">
+                                        <div class="card-body">
+                                          <h5 class="card-title">${book.title}</h5>
+                                          <p class="card-text">Price: $${book.price.toFixed(2)}</p>
+                                          ${book.quantity > 0 ? '<p class="card-text text-success">In Stock</p>' : '<p class="card-text text-danger">Out of Stock</p>'}
+                                        </div>
+                                      </div>
+                                    </li>
+                                  `;
+        bookList.appendChild(listItem);
+      });
+    })
+    .catch(error => console.error('Error fetching search results:', error));
+}
