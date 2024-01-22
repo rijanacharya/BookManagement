db4fcc87ae7e83d21920ce138430fd6ff0f9aaab
Rijan Acharya <rijanacharya100@gmail.com>
2024-01-22 13:20:46 -0500

    deleting extra files

--- Server/public/script/accordian.js
+++
@@ -1,6 +0,0 @@
-// Toggle plus and minus signs on accordion collapse
-$(document).ready(function () {
-  $(".accordion-button").click(function () {
-    $(this).toggleClass("collapsed");
-  });
-});
