2a3d01fbc492465bc3759b01e9196f7d44939aa7
Rijan Acharya <rijanacharya100@gmail.com>
2024-01-22 16:34:31 -0500

    deleting extra files

--- public/script/faq.js
+++
@@ -1,9 +0,0 @@
-function toggleAnswer(element) {
-  element.classList.toggle("active");
-  var answer = element.nextElementSibling;
-  if (element.classList.contains("active")) {
-    answer.style.maxHeight = answer.scrollHeight + "px";
-  } else {
-    answer.style.maxHeight = 0;
-  }
-}
--- public/script/hero_animation.js
+++
@@ -1,9 +0,0 @@
-function animateHero() {
-  const heroContent = document.querySelector(".hero-content");
-  const heroImage = document.querySelector(".hero-image");
-
-  heroContent.classList.add("animate");
-  heroImage.classList.add("animate");
-}
-
-window.addEventListener("load", animateHero);
